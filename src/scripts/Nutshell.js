5e9ee9fce7439a7e9b6ddba8a95c06200b8cf992
Jacob Eckert <jweckert17@gmail.com>
2020-08-26 09:32:50 -0500

    more helpful console log for testing in nutshell

--- src/scripts/Nutshell.js
+++ src/scripts/Nutshell.js
@@ -1,4 +1,4 @@
 export const Nutshell = () => {
     // Render all your UI components here
-    console.log("authenticated... nutshell has now been called")
+    console.log(`logged in to nutshell as user ${sessionStorage.getItem("activeUser")}`)
 }
