e78c2c0b1fd13553619f6da886a36aa85df5074e
Jacob Eckert <jweckert17@gmail.com>
2020-08-26 11:58:51 -0500

    rendering dialog modal in message for if you click on a username to add as friend

--- src/scripts/Messages/Message.js
+++ src/scripts/Messages/Message.js
@@ -3,9 +3,24 @@ import { deleteMessage } from "./MessageDataProvider.js"
 const eventHub = document.querySelector(".container")
 
 eventHub.addEventListener("click", event => {
-  if(event.target.id.startsWith("deleteMessage--")) {
-    const messageId = event.target.id.split("--")[1]
-    deleteMessage(messageId)
+  const [ prefix, id ] = event.target.id.split("--")
+
+  if(prefix === "deleteMessage") {
+    deleteMessage(id)
+  }
+
+  else if(prefix === "openAddFriendDialog") {
+    const dialogNode = document.querySelector(`#addFriendDialog--${id}`)
+    dialogNode.showModal()
+  }
+
+  else if(prefix === "closeAddFriendDialog") {
+    const dialogNode = document.querySelector(`#addFriendDialog--${id}`)
+    dialogNode.close()
+  }
+
+  else if(prefix === "addFriend") {
+    // saveFriend(id) // will eventually be written in FriendProvider and will save the friend with the given ID as a friend of the currently logged in user
   }
 })
 
@@ -14,18 +29,27 @@ export const Message = messageObj => {
 
   return `
     <div class="message">
-      <p id="chatMessage--${userId}">${user.username}:</p>
-      <p>${message}</p>
+      <button class="message__username" id="openAddFriendDialog--${userId}">${user.username}:</button>
+      <p class="message__text">${message}</p>
       ${ messageDeleteButton(id, userId) }
+
+      <dialog class="dialog friend-dialog" id="addFriendDialog--${userId}">
+        <p class="friend-dialog__prompt">Would you like to add ${user.username} as a friend?</p>
+        <button class="addFriendButton" id="addFriend--${userId}">Yes</button>
+        <button class="closeDialogButton" id="closeAddFriendDialog--${userId}">Cancel</button>
+      </dialog>
     </div>
   `
 }
 
+/**
+ * If the given userId matches the userId of the activeUser, then return a button that will allow the user to delete the message... otherwise they should not be able to delete the message so just return an empty string
+ */
 const messageDeleteButton = (messageId, userId) => {
   const activeUser = parseInt(sessionStorage.getItem("activeUser"))
   if(userId === activeUser) {
     return `
-      <button id="deleteMessage--${messageId}">Delete Message</button>
+      <button class="message__deleteButton" id="deleteMessage--${messageId}">Delete Message</button>
     `
   }
   return "";
