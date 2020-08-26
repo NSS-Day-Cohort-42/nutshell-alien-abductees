698b7bd42e54e4bbde5d2d6d715f35c0713b437b
estherviolin <esthernaomi.violin@gmail.com>
2020-08-26 13:16:14 -0500

    saving provider before merging new master

--- src/scripts/Friends/FriendDataProvider.js
+++ src/scripts/Friends/FriendDataProvider.js
@@ -8,4 +8,10 @@ export const getFriends = () => {
         .then(res => res.json())
         .then(parsedFriends => {
             friends = parsedFriends
-            )}
+        })
+
+}
+
+export const useFriends = () => {
+    return friends.slice()
+}
