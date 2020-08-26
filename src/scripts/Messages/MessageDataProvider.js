059fa0088ac1c3dc0d618845a013574a1118185b
Jacob Eckert <jweckert17@gmail.com>
2020-08-26 10:54:14 -0500

    created messages data provider

---
+++ src/scripts/Messages/MessageDataProvider.js
@@ -0,0 +1,42 @@
+let messages = []
+
+const eventHub = document.querySelector(".container")
+
+const broadcastMessagesStateChanged = () => {
+  const messagesStateChangedEvent = new CustomEvent("messagesStateChanged")
+  eventHub.dispatchEvent(messagesStateChangedEvent)
+}
+
+export const getMessages = () => {
+  return fetch("http://localhost:8088/messages")
+    .then(res => res.json())
+    .then(messagesData => messages = messagesData)
+}
+
+export const useMessages = () => messages.slice()
+
+export const saveMessage = message => {
+  const messageObj = {
+    message: message,
+    timestamp: Date.now(),
+    userId: parseInt(sessionStorage.getItem("activeUser"))
+  }
+
+  return fetch("http://localhost:8088/messages", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(messageObj)
+  })
+    .then(getMessages)
+    .then(broadcastMessagesStateChanged)
+}
+
+export const deleteMessage = messageId => {
+  return fetch(`http://localhost:8088/messages/${messageId}`, {
+    method: "DELETE"
+  })
+    .then(getMessages)
+    .then(broadcastMessagesStateChanged)
+}
