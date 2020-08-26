bd80a750bc279a8fb49eb35a5e9ebde8822657e2
Jacob Eckert <jweckert17@gmail.com>
2020-08-26 13:52:47 -0500

    newsForm renders self on createNewsStory, wipes self out on submit

--- src/scripts/Nutshell.js
+++ src/scripts/Nutshell.js
@@ -3,20 +3,15 @@ import { TaskList } from "./Tasks/TaskList.js"
 import { eventList } from "./Events/EventsList.js"
 import { MessageList } from "./Messages/MessageList.js"
 import { NewsList } from "./News/NewsList.js"
-import { NewsForm } from "./News/NewsForm.js"
 import "./Tasks/TaskForm.js"
 import "./Events/EventForm.js"
+import "./News/NewsForm.js"
 
 export const Nutshell = () => {
-    // Render all your UI components here
-    console.log(`logged in to nutshell as user ${sessionStorage.getItem("activeUser")}`)
-
     NavBar();
 
     MessageList()
     TaskList()
     eventList()
-
     NewsList()
-    NewsForm()
 }
