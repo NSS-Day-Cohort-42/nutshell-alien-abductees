e7cbb2add74314c3f37173118a56f325d5b59cdf
estherviolin <esthernaomi.violin@gmail.com>
2020-08-31 11:49:12 -0500

    added border to weather, cleared current login after log out

--- src/scripts/auth/Logout.js
+++ src/scripts/auth/Logout.js
@@ -1,5 +1,10 @@
+//Esther Sanders ... renders a logout button after user logs in, dispatches click event to main.js when user clicks button, returns user to login/ registration form
+
+
+
 const contentTarget = document.querySelector(".logoutButtonContainer")
 const eventHub = document.body
+const loggedInAs = document.querySelector(".currentLogin")
 
 eventHub.addEventListener("click", clickEvent => {
     if(clickEvent.target.id==="logoutButton") {  
@@ -7,6 +12,7 @@ eventHub.addEventListener("click", clickEvent => {
         eventHub.dispatchEvent(logoutClicked)
         console.log("log out clicked")
         contentTarget.innerHTML = ""
+        loggedInAs.innerHTML = ""
     }
 })
 
